fef390b396cfcda4046968282582428ec1d1e7d4
Jesus Marenco <jesusmarencodev@gmail.com>
2023-08-11 11:03:17 -0500

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'redux-app';
+  counter?:number; 
+
+  constructor(){
+    this.counter = 10;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SonComponent } from './counter/son/son.component';
+import { GrandsonComponent } from './counter/grandson/grandson.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SonComponent,
+    GrandsonComponent,
   ],
   imports: [
     BrowserModule,
